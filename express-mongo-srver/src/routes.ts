43240d58d38e7e8bb62688bf2d1bb02308e400a2
technologicaljerry <cserajatverma@gmail.com>
2024-05-07 21:17:54 +0530

    Delete routes.ts

--- express-mongo-srver/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express, Request, Response } from "express";
-
-function routes(server: Express) {
-
-    server.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
-
-}
-
-export default routes;
